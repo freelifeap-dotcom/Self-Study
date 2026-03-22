f74e2371644b9dd326791ce080127747e8a1097b
Paulau Artsem <freelife.ap@gmail.com>
2026-03-22 17:03:46 +0300

    Finalize Selenium: v4 Add auto-screenshots, txt-logs and error handling

--- Selenium_v4/POM/first_progect/pages/base_page.py
+++ Selenium_v4/POM/first_progect/pages/base_page.py
@@ -14,13 +14,13 @@ class BasePage:
     def find_clickable_element(self, locator, time=10):
         return WebDriverWait(self.driver, time).until(
             EC.element_to_be_clickable(locator),
-            message=f"Не вижу элемент: {locator}"
+            message=f"❌ Не смог найти элемент {locator} на странице {self.driver.current_url}"
         )
 
     def find_element(self, locator, time=10):
         return WebDriverWait(self.driver, time).until(
             EC.presence_of_element_located(locator),
-            message=f"Не вижу элемент: {locator}"
+            message=f"❌ Не смог найти элемент {locator} на странице {self.driver.current_url}"
         )
 
     # 3. УЧИМ СТРАНИЦУ ОТКРЫВАТЬСЯ
--- Selenium_v4/POM/first_progect/tests/test_ebay.py
+++ Selenium_v4/POM/first_progect/tests/test_ebay.py
@@ -9,8 +9,8 @@ def test_ebay_search(browser):
     ebay_page.search_item("iPhone 15")
     ebay_page.click_search()
     result_text = ebay_page.get_first_result_text()
-    assert "iPhone" in result_text
+    assert "iPhone" in result_text, f"❌ Ожидали iPhone в результатах, но получили:'{result_text[:50]}...'"
     print(f"Тест пройден! Нашелся товар: {result_text}")
 
 
-    time.sleep(3)
+    time.sleep(2)
